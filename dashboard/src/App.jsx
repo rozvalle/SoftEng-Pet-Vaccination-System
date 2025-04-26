1762386fc5edb7f7c5119fa41bb8584b87b213ca
Elmo <hed-fevaleros@smu.edu.ph>
2025-04-27 01:50:01 +0800

    feat: add route for managing vaccinations in the main application

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -8,6 +8,7 @@ import ManagePets from "./pages/ManagePets";
 import Users from "./pages/Users";
 import Pets from "./pages/Pets";
 import Vaccines from "./pages/Vaccines";
+import Vaccinations from "./pages/ManageVaccinations";
 
 const PrivateRoute = () => {
   const token = sessionStorage.getItem("isAuthenticated");
@@ -28,6 +29,7 @@ export default function App() {
             <Route path="users/:id" element={<Users />} />
             <Route path="pets/:id" element={<Pets />} />
             <Route path="vaccines/:id" element={<Vaccines />} />
+            <Route path="vaccinations" element={<Vaccinations />} />
           </Route>
         </Route>
         <Route path="/login" element={<Login />} />
